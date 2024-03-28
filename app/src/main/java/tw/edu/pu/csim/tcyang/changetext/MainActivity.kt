91966f8f7bf8aa6d3d0a82c05f94b4f2355ad98d
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-03-28 10:59:00 +0800

    V2_短按放大字體

--- app/src/main/java/tw/edu/pu/csim/tcyang/changetext/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/changetext/MainActivity.kt
@@ -2,10 +2,29 @@ package tw.edu.pu.csim.tcyang.changetext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    lateinit var btnA: Button
+    lateinit var txv: TextView
+    var size: Float = 20f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv = findViewById(R.id.txv)
+        txv.textSize = size
+
+        btnA = findViewById(R.id.btnA)
+        btnA.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        size++
+        txv.textSize = size
     }
 }
